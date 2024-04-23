c75fa1220ace7617cb90e1445a5c73c3e4d33914
sarveshshreekumaran <sarveshshreekumaran@gmail.com>
2024-04-23 13:42:15 +0530

    Added reset password form

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import ForgotPassword from "./components/ForgotPassword";
+import ResetPassword from "./components/ResetPassword";
 
 import Layout from "./components/Layout";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -24,6 +25,10 @@ function App() {
         {
           path: "/forgot_password",
           element: <ForgotPassword />,
+        },
+        {
+          path: "/reset_password",
+          element: <ResetPassword />,
         },
       ],
     },
--- client/src/components/ResetPassword.jsx
+++ client/src/components/ResetPassword.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
 
-function ForgotPassword() {
-  const [email, setEmail] = useState("");
-  const navigate = useNavigate();
+function ResetPassword() {
+  const [passwordResetForm, setPasswordResetForm] = useState({
+    otp: "",
+    newPassword: "",
+  });
+
+  const { otp, newPassword } = passwordResetForm;
 
   const onChange = (e) => {
-    setEmail(e.target.value);
+    setPasswordResetForm((prevInput) => ({
+      ...prevInput,
+      [e.target.name]: e.target.value,
+    }));
   };
 
   const onSubmit = (e) => {
@@ -21,10 +27,9 @@ function ForgotPassword() {
       });
       return response.json();
     };
-    registerUser("http://localhost:4000/user/forgot_password", {
-      email: email,
+    registerUser("http://localhost:4000/user/reset_password", {
+      ...passwordResetForm,
     }).then((data) => {
-      navigate("/reset_password");
       console.log(data);
     });
   };
@@ -38,7 +43,7 @@ function ForgotPassword() {
           alt="Your Company"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Forgot Password
+          Reset Password
         </h2>
       </div>
 
@@ -46,19 +51,41 @@ function ForgotPassword() {
         <form className="space-y-6" onSubmit={onSubmit}>
           <div>
             <label
-              htmlFor="email"
+              htmlFor="otp"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
-              Email address
+              OTP
             </label>
             <div className="mt-2">
               <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
+                id="otp"
+                name="otp"
+                type="number"
                 required
-                value={email}
+                value={otp}
+                onChange={onChange}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="newPassword"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                New Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="newPassword"
+                name="newPassword"
+                type="password"
+                autoComplete="current-password"
+                required
+                value={newPassword}
                 onChange={onChange}
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -70,23 +97,13 @@ function ForgotPassword() {
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Forgot Password
+              Reset Password
             </button>
           </div>
         </form>
-
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Not registered?
-          <Link
-            to="/register"
-            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >
-            Register
-          </Link>
-        </p>
       </div>
     </div>
   );
 }
 
-export default ForgotPassword;
+export default ResetPassword;
