aaaf2eb39a1dc5f6fe07b6645c7e76432ffedeb6
sarveshshreekumaran <sarveshshreekumaran@gmail.com>
2024-04-24 11:20:01 +0530

    Added read update delete controllers for file route

--- server/controllers/fileControllers.js
+++ server/controllers/fileControllers.js
@@ -12,8 +12,7 @@ const uploadFile = (req, res) => {
     { public_id: "wizinoa" },
     async (error, result) => {
       if (error) {
-        console.log(error);
-        res.json({ Error: error.message });
+        return res.json({ Error: error.message });
       }
       console.log(result);
       const file = await File.create({
@@ -25,6 +24,46 @@ const uploadFile = (req, res) => {
   );
 };
 
+const getFile = async (req, res) => {
+  const file = await File.find({});
+  res.json(file);
+};
+
+const updateFile = async (req, res) => {
+  const id = req.params.id;
+  const file = await File.findOne({ _id: id });
+  if (!file) {
+    return res.json({ message: "File not found!" });
+  }
+  cloudinary.v2.uploader.upload(
+    req.file.path,
+    { public_id: "wizinoa" },
+    async (error, result) => {
+      if (error) {
+        return res.json({ Error: error.message });
+      }
+      console.log(result);
+      const file = await File.findByIdAndUpdate(
+        { _id: id },
+        {
+          name: `${result.original_filename}`,
+          fileURL: `${result.secure_url}`,
+        },
+        { new: true }
+      );
+      res.json({ message: "File updated successfully", file });
+    }
+  );
+};
+const deleteFile = async (req, res) => {
+  const id = req.params.id;
+  const file = await File.findByIdAndDelete({ _id: id });
+  res.json({ message: "File deleted succesfully", file });
+};
+
 module.exports = {
   uploadFile,
+  getFile,
+  updateFile,
+  deleteFile,
 };
--- server/routes/fileRoutes.js
+++ server/routes/fileRoutes.js
@@ -3,7 +3,13 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
-const { uploadFile } = require("../controllers/fileControllers");
-router.post("/", upload.single("file"), uploadFile);
+const {
+  uploadFile,
+  getFile,
+  updateFile,
+  deleteFile,
+} = require("../controllers/fileControllers");
+router.route("/").get(getFile).post(upload.single("file"), uploadFile);
+router.route("/:id").put(upload.single("file"), updateFile).delete(deleteFile);
 
 module.exports = router;
