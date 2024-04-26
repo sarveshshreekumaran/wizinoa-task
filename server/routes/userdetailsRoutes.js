557f614fda02d2162d44ee7bd6fad9c8463177b6
sarveshshreekumaran <sarveshshreekumaran@gmail.com>
2024-04-26 12:36:48 +0530

    Removed useragent package, added os module, updted userdetailsRoutes

--- server/routes/userdetailsRoutes.js
+++ server/routes/userdetailsRoutes.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const os = require("os");
 
 router.get("/", (req, res) => {
-  const userdetails = req.useragent;
-  res.json(userdetails);
+  const userdetails = {
+    version: os.version(),
+    type: os.type(),
+    release: os.release(),
+    hostname: os.hostname(),
+    cpus: os.cpus()[0].model,
+    totalmem: os.totalmem(),
+    userInfo: os.userInfo(),
+    headers: req.headers,
+    ip: req.ip,
+  };
+  console.log();
+  res.json({ userdetails });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,13 +3,11 @@ const connectDB = require("./configs/dbConn");
 connectDB();
 const express = require("express");
 const cors = require("cors");
-const useragent = require("express-useragent");
 const port = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(useragent.express());
 app.listen(port, () => {
   console.log(`Server listing on ${port}`);
 });
